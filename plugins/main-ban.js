bc9541ef55828b812b9abb061e5b91622f7ae421
DUA FATIMA <policeduafatima@gmail.com>
2025-08-30 16:01:53 +0500

    Update main-ban.js

--- plugins/main-ban.js
+++ plugins/main-ban.js
@@ -29,7 +29,7 @@ cmd({
         fs.writeFileSync("./lib/ban.json", JSON.stringify([...new Set(banned)], null, 2));
 
         await conn.sendMessage(from, {
-            image: { url: "https://files.catbox.moe/7zfdcq.jpg" },
+            image: { url: "https://files.catbox.moe/iqxgwv.jpg" },
             caption: `⛔ User has been banned from using the bot.`
         }, { quoted: mek });
 
@@ -66,7 +66,7 @@ cmd({
         fs.writeFileSync("./lib/ban.json", JSON.stringify(updated, null, 2));
 
         await conn.sendMessage(from, {
-            image: { url: "https://files.catbox.moe/7zfdcq.jpg" },
+            image: { url: "https://files.catbox.moe/iqxgwv.jpg" },
             caption: `✅ User has been unbanned.`
         }, { quoted: mek });
 
