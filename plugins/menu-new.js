a4753ecfd4f6597b71c813b3156e44e6e23cba42
DUA FATIMA <policeduafatima@gmail.com>
2025-09-02 23:17:35 +0500

    Update menu-new.js

--- plugins/menu-new.js
+++ plugins/menu-new.js
@@ -5,9 +5,9 @@ const { runtime } = require('../lib/functions');
 const axios = require('axios');
 
 cmd({
-    pattern: "menu",
+    pattern: "fatima",
     desc: "Show interactive menu system",
-    category: "menu",
+    category: "fatima",
     react: "🧾",
     filename: __filename
 }, async (conn, mek, m, { from, reply }) => {
