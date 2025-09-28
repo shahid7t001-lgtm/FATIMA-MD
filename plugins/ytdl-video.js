a9e987dd59f2471e4b08a04dd528267c08f6971a
DUA FATIMA <policeduafatima@gmail.com>
2025-09-28 18:34:47 +0500

    Update ytdl-video.js

--- plugins/ytdl-video.js
+++ plugins/ytdl-video.js
@@ -32,7 +32,7 @@ cmd({
         await reply("⏳ Downloading video...");
 
         // Use API to get video
-        const apiUrl = `https://apis.davidcyriltech.my.id/download/ytmp4?url=${encodeURIComponent(videoUrl)}`;
+        const apiUrl = `https://jawad-tech.vercel.app/download/ytmp4?url=${encodeURIComponent(videoUrl)}`;
         const response = await fetch(apiUrl);
         const data = await response.json();
 
