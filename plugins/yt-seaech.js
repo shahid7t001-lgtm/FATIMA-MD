206c985a51ebf31707a6b464edd39961f3aa1e05
DUA FATIMA <policeduafatima@gmail.com>
2025-09-29 19:07:27 +0500

    Update yt-seaech.js

--- plugins/yt-seaech.js
+++ plugins/yt-seaech.js
@@ -9,7 +9,7 @@ const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, 
 cmd({
     pattern: "yts",
     alias: ["ytsearch"],
-    use: '.yts irfan',
+    use: '.yts fatima',
     react: "🔎",
     desc: "Search and get details from youtube.",
     category: "search",
