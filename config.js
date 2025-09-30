a9e48facdaec99d1e984a97e97a1b3344d148e2f
shahid7t001-lgtm <shahid7t001@gmail.com>
2025-09-30 12:43:33 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "IK~H9hj0BLS#qX26B-SpnpjYrnovyYwcbuMTRC-2UH5cOhk1qe8uSps",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -13,7 +13,7 @@ AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 // make true if you want auto reply on status 
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
 // make true if you want auto reply on status 
-AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY FATIMA-MD 🤍*",
+AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY VENOM-MD 🤍*",
 // set the auto reply massage on status reply  
 ANTI_DELETE: process.env.ANTI_DELETE || "true",
 // set true false for anti delete     
@@ -27,15 +27,15 @@ ANTI_LINK: process.env.ANTI_LINK || "true",
 // make anti link true,false for groups 
 MENTION_REPLY: process.env.MENTION_REPLY || "false",
 // make true if want auto voice reply if someone menetion you 
-MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/9frdhs.jpg",
+MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/03m3e1.jpg",
 // add custom menu and mention reply image url
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
-BOT_NAME: process.env.BOT_NAME || "FATIMA-MD",
+BOT_NAME: process.env.BOT_NAME || "─ ͟͟͞͞➳❥ Vɛη͜͡ɵɱ ➺☠️🩸⇣",
 // add bot namw here for menu
-STICKER_NAME: process.env.STICKER_NAME || "FATIMA-MD",
+STICKER_NAME: process.env.STICKER_NAME || "VENOM-MD",
 // type sticker pack name 
-CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
+CUSTOM_REACT: process.env.CUSTOM_REACT || "true",
 // make this true for custum emoji react    
 CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
 // chose custom react emojis by yourself 
@@ -45,15 +45,15 @@ OWNER_NUMBER: process.env.OWNER_NUMBER || "923155641171",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "𝐸𝑅𝐹𝒜𝒩 𝒜𝐻𝑀𝒜𝒟",
 // add bot owner name
-DESCRIPTION: process.env.DESCRIPTION || "*© CREATER FATIMA-MD *",
+DESCRIPTION: process.env.DESCRIPTION || "*© CREATER VENOM-MD *",
 // add bot owner name    
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/9frdhs.jpg",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/03m3e1.jpg",
 // add img for alive msg
-LIVE_MSG: process.env.LIVE_MSG || "> HEY IM ALIVE NOW  *FATIMA-MD*⚡",
+LIVE_MSG: process.env.LIVE_MSG || "> HEY IM ALIVE NOW  *VENOM-MD*⚡",
 // add alive msg here 
 READ_MESSAGE: process.env.READ_MESSAGE || "false",
 // Turn true or false for automatic read msgs
-AUTO_REACT: process.env.AUTO_REACT || "false",
+AUTO_REACT: process.env.AUTO_REACT || "true",
 // make this true or false for auto react on all msgs
 ANTI_BAD: process.env.ANTI_BAD || "false",
 // false or true for anti bad words  
